573db38c3c3d2f57d7eb18e7c2d1c039c2136e91
Gent Veseli <gent.veseli1@gmail.com>
2024-06-11 13:49:41 +0000

    1st

--- server.js
+++ server.js
@@ -58,3 +58,6 @@ const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Node.js listening on port " + listener.address().port);
 });
 
+console.log("Hello World");
+
+const app = require('./myApp');
